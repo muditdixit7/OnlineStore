deaef5aebd2048fb1aa08aa061aad68f8df5d6e5
muditdixit7 <muditdixit7@gmail.com>
2016-08-15 20:09:31 +0530

    upgraded nodejs version

--- configuration/config.js
+++ configuration/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongodb: {
-		url: "mongodb://localhost:27017/OnlineStore"
+		url: "mongodb://localhost:27017/OnlineStore"//mongodb://<dbuser>:<dbpassword>@ds161315.mlab.com:61315/heroku_p9bs95z7
 	}
 }
